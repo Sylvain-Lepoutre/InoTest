dd11dc2a1cfac3f534f25427c8319c5334cf0675
matthieu <georgematthieu1@gmail.com>
2023-05-25 12:07:35 +0200

    added a focus change function for the buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <header>
-        <NavBar title="InoLab" />
+        <NavBar title="Inotest" />
       </header>
       <div className="flex flex-col lg:justify-between items-center justify-center lg:items-start">
         <h2 className=" md:max-w-min text-3xl md:block lg:block xl:block md:text-4xl xl:text-5xl lg:text-5xl md:ml-[35rem] lg:ml-[45rem] xl:mt-[-3.5rem] xl:ml-[59rem]">
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="md:grid md:grid-cols-2 md:grid-rows-2 md:mt-32 flex justify-center w-screen mt-60">
         <a
           href="/getstarted"
-          className="bg-black focus:outline-none focus:border-slate-700 focus:ring-2 focus:ring-slate-700 text-white w-[15rem] rounded-lg h-[3rem] md:col-start-2 md:row-start-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex justify-center items-center"
+          className="bg-black text-white w-[15rem] rounded-lg h-[3rem] md:col-start-2 md:row-start-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex justify-center items-center"
         >
           GET STARTED
         </a>
