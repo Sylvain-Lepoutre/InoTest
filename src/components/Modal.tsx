38503232301e9fb0fe140c5455934d69827c6093
matthieu <georgematthieu1@gmail.com>
2023-06-06 16:07:45 +0200

    minor fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import { useRef, useState } from "react";
+import useEscapeKey from "../hook/useEscapeKey";
 
 type ModalButtonProps = {
   buttonText: string;
   modalContent: string;
   style?: string;
-}
+};
 
 export default function ModalButton(props: ModalButtonProps) {
   const buttonText = props.buttonText !== undefined ? props.buttonText : "";
   const modalContent = props.modalContent !== undefined ? props.modalContent : "";
   const style = props.style !== undefined ? props.style : "";
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const escapeRef = useRef<HTMLElement>(null);
+
+  useEscapeKey(escapeRef);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -22,19 +26,20 @@ export default function ModalButton(props: ModalButtonProps) {
 
   return (
     <div className={style}>
-      <button
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-        onClick={openModal}
-      >
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={openModal}>
         {buttonText}
       </button>
 
       {isModalOpen && (
-        <dialog className="fixed inset-0 flex items-center justify-center z-50 border-none" aria-label='Accessibility error window'>
+        <dialog
+          className="fixed inset-0 flex items-center justify-center z-50 border-none"
+          aria-label="Accessibility error window"
+        >
           <div className="absolute inset-0 bg-gray-900 opacity-75"></div>
           <div className="bg-white p-4 rounded z-10">
             <p>{modalContent}</p>
             <button
+              ref={escapeRef}
               className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
               onClick={closeModal}
             >
@@ -45,4 +50,4 @@ export default function ModalButton(props: ModalButtonProps) {
       )}
     </div>
   );
-};
+}
