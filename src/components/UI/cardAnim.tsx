6b82c55f38c27b8124b10a99ef58dac0aa96db14
matthieu <georgematthieu1@gmail.com>
2023-06-08 15:19:17 +0200

    merge

--- src/components/UI/cardAnim.tsx
+++ src/components/UI/cardAnim.tsx
@@ -1,23 +1,23 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect, MouseEvent, TouchEvent } from "react";
 
 const ImageSlider = () => {
-  const trackRef = useRef(null);
-  const prevPercentageRef = useRef(0);
-  const mouseDownAtRef = useRef(0);
+  const trackRef: RefObject<HTMLElement> = useRef(null);
+  const prevPercentageRef = useRef<number>(0);
+  const mouseDownAtRef = useRef<number>(0);
   const minPercentage = -100;
   const maxPercentage = 0;
 
   useEffect(() => {
-    const handleOnDown = (e) => {
+    const handleOnDown = (e: MouseEvent) => {
       mouseDownAtRef.current = e.clientX;
     };
 
     const handleOnUp = () => {
       mouseDownAtRef.current = 0;
-      prevPercentageRef.current = trackRef.current.dataset.percentage;
+      prevPercentageRef.current = parseFloat(trackRef.current!.dataset.percentage || "0");
     };
 
-    const handleOnMove = (e) => {
+    const handleOnMove = (e: MouseEvent) => {
       if (mouseDownAtRef.current === 0) return;
 
       const mouseDelta = parseFloat(mouseDownAtRef.current) - e.clientX;
@@ -34,30 +34,30 @@ const ImageSlider = () => {
         nextPercentage = nextPercentage < minPercentage ? maxPercentage + overflow : minPercentage - overflow;
       }
 
-      trackRef.current.dataset.percentage = nextPercentage;
+      trackRef.current!.dataset.percentage = nextPercentage.toString();
 
-      trackRef.current.style.transform = `translate(${nextPercentage}%, -50%)`;
+      trackRef.current!.style.transform = `translate(${nextPercentage}%, -50%)`;
 
-      const images = trackRef.current.getElementsByClassName('image');
-      for (const image of images) {
-        image.style.objectPosition = `${100 + nextPercentage}% center`;
+      const images = trackRef.current!.getElementsByClassName("image");
+      for (const image of Array.from(images)) {
+        image.setAttribute("style", `object-position: ${100 + nextPercentage}% center`);
       }
     };
 
-    window.addEventListener('mousedown', handleOnDown);
-    window.addEventListener('touchstart', (e) => handleOnDown(e.touches[0]));
-    window.addEventListener('mouseup', handleOnUp);
-    window.addEventListener('touchend', (e) => handleOnUp(e.touches[0]));
-    window.addEventListener('mousemove', handleOnMove);
-    window.addEventListener('touchmove', (e) => handleOnMove(e.touches[0]));
+    window.addEventListener("mousedown", handleOnDown);
+    window.addEventListener("touchstart", (e: TouchEvent) => handleOnDown(e.touches[0]));
+    window.addEventListener("mouseup", handleOnUp);
+    window.addEventListener("touchend", (e: TouchEvent) => handleOnUp(e.touches[0]));
+    window.addEventListener("mousemove", handleOnMove);
+    window.addEventListener("touchmove", (e: TouchEvent) => handleOnMove(e.touches[0]));
 
     return () => {
-      window.removeEventListener('mousedown', handleOnDown);
-      window.removeEventListener('touchstart', (e) => handleOnDown(e.touches[0]));
-      window.removeEventListener('mouseup', handleOnUp);
-      window.removeEventListener('touchend', (e) => handleOnUp(e.touches[0]));
-      window.removeEventListener('mousemove', handleOnMove);
-      window.removeEventListener('touchmove', (e) => handleOnMove(e.touches[0]));
+      window.removeEventListener("mousedown", handleOnDown);
+      window.removeEventListener("touchstart", (e: TouchEvent) => handleOnDown(e.touches[0]));
+      window.removeEventListener("mouseup", handleOnUp);
+      window.removeEventListener("touchend", (e: TouchEvent) => handleOnUp(e.touches[0]));
+      window.removeEventListener("mousemove", handleOnMove);
+      window.removeEventListener("touchmove", (e: TouchEvent) => handleOnMove(e.touches[0]));
     };
   }, []);
 
@@ -73,53 +73,52 @@ const ImageSlider = () => {
         className="image image-animation w-40 h-56 object-cover object-center"
         src="https://images.unsplash.com/photo-1524781289445-ddf8f5695861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         draggable="false"
-        alt="Slider Image 1"
+        alt="Slider  1"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1610194352361-4c81a6a8967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
         draggable="false"
-        alt="Slider Image 2"
+        alt="Slider  2"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1618202133208-2907bebba9e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         draggable="false"
-        alt="Slider Image 3"
+        alt="Slider  3"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1495805442109-bf1cf975750b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         draggable="false"
-        alt="Slider Image 4"
+        alt="Slider  4"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1548021682-1720ed403a5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         draggable="false"
-        alt="Slider Image 5"
+        alt="Slider  5"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1496753480864-3e588e0269b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80"
         draggable="false"
-        alt="Slider Image 6"
+        alt="Slider  6"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1613346945084-35cccc812dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1759&q=80"
         draggable="false"
-        alt="Slider Image 7"
+        alt="Slider  7"
       />
       <img
         className="image w-40 h-56  object-cover object-center"
         src="https://images.unsplash.com/photo-1516681100942-77d8e7f9dd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         draggable="false"
-        alt="Slider Image 8"
+        alt="Slider  8"
       />
     </div>
   );
 };
 
 export default ImageSlider;
-
