ca9e69161a4e0a16acda10bcaf9c71c6ce43c587
matthieu <georgematthieu1@gmail.com>
2023-06-12 15:52:26 +0200

    fixed error in translation component

--- src/components/UI/LanguageSelector.tsx
+++ src/components/UI/LanguageSelector.tsx
@@ -1,7 +1,7 @@
-import {} from "react";
 import { useTranslation } from "react-i18next";
 import englishFlag from "/en-flag.png";
 import frenchFlag from "/fr-flag.png";
+import { RefObject, KeyboardEvent } from "react";
 
 type LanguageSelectorProps = {
   horizontalFocus: (event: KeyboardEvent) => void;
@@ -11,21 +11,22 @@ type LanguageSelectorProps = {
 function LanguageSelector(props: LanguageSelectorProps) {
   const { i18n } = useTranslation();
 
-  const changeLanguage = () => {
+  const changeLanguage = async () => {
     const currentLanguage = i18n.language;
     const newLanguage = currentLanguage === "en" ? "fr" : "en";
-    i18n.changeLanguage(newLanguage);
+    await i18n.changeLanguage(newLanguage);
   };
 
-  const flagImage = i18n.language === "en" ? frenchFlag : englishFlag;
-
+  const flagImage = i18n.language === "en" ? (frenchFlag as string) : (englishFlag as string);
   return (
     <div>
       <button
         ref={props.navRef}
-        onClick={changeLanguage}
+        onClick={() => {
+          void changeLanguage();
+        }}
         onKeyDown={props.horizontalFocus}
-        tabIndex="0"
+        tabIndex={0}
         aria-label="Change language"
       >
         <img className="w-8 mt-[1.7rem] mr-[1rem] md:mt-1 p-0" src={flagImage} alt="Flag" />
