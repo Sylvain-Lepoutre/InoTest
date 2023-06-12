ce471cb14f743c6ee298fc4bcbd634135ca55227
matthieu <georgematthieu1@gmail.com>
2023-06-12 13:24:53 +0200

    carousel css

--- src/components/fakeComponents/FakeStepper.tsx
+++ src/components/fakeComponents/FakeStepper.tsx
@@ -1,6 +1,6 @@
 import ErrorModalButton from "@components/UI/ErrorModal";
 import React, { useState } from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import i18n from "../../../i18n";
 
 interface FakeStep {
@@ -8,26 +8,32 @@ interface FakeStep {
   text?: string;
 }
 
-interface FakeStepperProps {
-  container: string;
-  href: string;
-  style: string;
-  style2: string;
-  styledImage: string;
-  styledButtons: string;
-  styledButtons2: string;
-}
-
-const FakeStepper: React.FC = (props: FakeStepperProps) => {
+const FakeStepper = () => {
   const { t } = useTranslation();
   i18n.language;
   const [activeStep, setActiveStep] = useState<number>(0);
 
   const steps: FakeStep[] = [
-    { image: "https://picsum.photos/id/116/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 1" },
-    { image: "https://picsum.photos/id/196/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 2" },
-    { image: "https://picsum.photos/id/176/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 3" },
-    { image: "https://picsum.photos/id/184/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 4" },
+    {
+      image:
+        "https://images.unsplash.com/photo-1456926631375-92c8ce872def?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 1",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1744&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 2",
+    },
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1666879952402-a53a92b5884f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 3",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1682695798256-28a674122872?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 4",
+    },
   ];
 
   const handleNext = (array: FakeStep[]) => {
@@ -41,53 +47,58 @@ const FakeStepper: React.FC = (props: FakeStepperProps) => {
   };
 
   return (
-    <div className={`${props.container}`}>
-      <div className={`${props.style}`}>
-        <ErrorModalButton
-          buttonText="!"
-          modalContent={t('stepper-error')}
-          style="text-black"
-        />
-        <div className={`${props.style2}`}>
+    <div className="flex flex-col items-center p-4">
+      <div className="md:w-[40rem] bg-transparent rounded-lg">
+        <div className="flex flex-col items-center ">
+          <div className="mb-4 flex ">
+            <img src={steps[activeStep].image} alt="" className="scale-75 object-cover" />
+            <div className="flex items-center justify-between p-4">
+              <ErrorModalButton buttonText="!" modalContent={t("stepper-error")} style="text-black" />
+            </div>
+          </div>
           <div>
-            <img className={`${props.styledImage}`} src={steps[activeStep].image} alt="" />
+            <p className="text-center md:-mt-[2rem]">{steps[activeStep].text}</p>
           </div>
-          <div className="mt-6">
-            <p className="text">{steps[activeStep].text}</p>
-          </div>
+          <ErrorModalButton
+            buttonText="!"
+            modalContent={t("stepper-error")}
+            style="md:hidden text-black flex items-center justify-between p-4"
+          />
         </div>
-        <button
-          onClick={() => {
-            handlePrevious();
-          }}
-          className={`${props.styledButtons}`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-4 h-4"
+        <div className="flex justify-between md:-mt-[2rem]">
+          <button
+            onClick={() => {
+              handlePrevious();
+            }}
+            className="md:place-self-center  flex items-center buttonClass2 justify-center px-4 py-2 rounded-md hover:-translate-x-1 transform md:transition-transform duration-200 md:mr-[20vw]"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 5l-7 7m0 0l7 7m-7-7h18" />
-          </svg>
-        </button>
-        <button
-          onClick={() => {
-            handleNext(steps);
-          }}
-          className={`${props.styledButtons2}`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-4 h-4"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-4 h-4"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 5l-7 7m0 0l7 7m-7-7h18" />
+            </svg>
+          </button>
+          <button
+            onClick={() => {
+              handleNext(steps);
+            }}
+            className="md:place-self-center  flex items-center buttonClass2 justify-center px-4 py-2 rounded-md hover:translate-x-1 transform md:transition-transform duration-200 md:ml-[20vw]"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-4 h-4"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/trueComponents/TrueStepper.tsx
+++ src/components/trueComponents/TrueStepper.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useRef, RefObject } from "react";
+import { useState, useRef, RefObject } from "react";
 import useFocus from "../../hook/useFocus";
 import RightModalButton from "@components/UI/RightModal";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import i18n from "../../../i18n";
 
 interface TrueStep {
@@ -9,18 +9,7 @@ interface TrueStep {
   text?: string;
 }
 
-interface TrueStepperProps {
-  container: string;
-  href: string;
-  style: string;
-  style2: string;
-  style3: string;
-  styledImage: string;
-  styledButtons: string;
-  styledButtons2: string;
-}
-
-const Stepper: React.FC = (props: TrueStepperProps) => {
+const Stepper = () => {
   const { t } = useTranslation();
   i18n.language;
 
@@ -32,10 +21,26 @@ const Stepper: React.FC = (props: TrueStepperProps) => {
   ];
 
   const steps: TrueStep[] = [
-    { image: "https://picsum.photos/id/116/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 1" },
-    { image: "https://picsum.photos/id/196/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 2" },
-    { image: "https://picsum.photos/id/176/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 3" },
-    { image: "https://picsum.photos/id/184/1500/530", text: "lorem ipsum dolor fatem si vis pacem para bellum 4" },
+    {
+      image:
+        "https://images.unsplash.com/photo-1456926631375-92c8ce872def?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 1",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1744&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 2",
+    },
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1666879952402-a53a92b5884f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 3",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1682695798256-28a674122872?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      text: "lorem ipsum dolor fatem si vis pacem para bellum 4",
+    },
   ];
 
   const { horizontalFocus } = useFocus(buttonRefs);
@@ -52,71 +57,70 @@ const Stepper: React.FC = (props: TrueStepperProps) => {
 
   return (
     <>
-      <div className={`${props.container}`}>
-        <div className={`${props.style}`} role="tabpanel">
-          <RightModalButton
-            buttonText="✓"
-            modalContent={t('stepper-right')}
-            style="text-black"
-          />
-          <div className={`${props.style2}`}>
-            <img
-              className={`${props.styledImage}`}
-              src={steps[activeStep].image}
-              alt={`img ${activeStep + 1}`}
-              aria-hidden="true"
-              role="presentation"
-            />
-            <p className="text" aria-label={`text ${activeStep + 1}`}>
-              {steps[activeStep].text}
-            </p>
+      <div className="flex flex-col items-center p-4">
+        <div className="md:w-[40rem] bg-transparent rounded-lg">
+          <div className="flex flex-col items-center ">
+            <div className="mb-4 flex ">
+              <img src={steps[activeStep].image} alt="" className="scale-75 object-cover" />
+              <div className="flex items-center justify-between p-4">
+                <RightModalButton buttonText="!" modalContent={t("stepper-right")} style="text-black" />
+              </div>
+            </div>
+            <div>
+              <p aria-label={`text ${activeStep + 1}`} className="text-center md:-mt-[2rem]">
+                {steps[activeStep].text}
+              </p>
+            </div>
+            <RightModalButton buttonText="!" modalContent={t("stepper-right")} style="text-black" />
           </div>
-          <button
-            ref={buttonRefs[0]}
-            aria-label="previous step button"
-            tabIndex={0}
-            type="button"
-            onClick={() => {
-              handlePrevious();
-            }}
-            onKeyDown={() => {
-              horizontalFocus(event);
-            }}
-            className={`${props.styledButtons}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-4 h-4"
+          <div className="flex justify-between md:-mt-[2rem]">
+            <button
+              ref={buttonRefs[0]}
+              aria-label="previous step button"
+              tabIndex={0}
+              type="button"
+              onClick={() => {
+                handlePrevious();
+              }}
+              onKeyDown={() => {
+                horizontalFocus(event);
+              }}
+              className="md:place-self-center  flex items-center buttonClass2 justify-center px-4 py-2 rounded-md hover:-translate-x-1 transform md:transition-transform duration-200 md:mr-[20vw]"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 5l-7 7m0 0l7 7m-7-7h18" />
-            </svg>
-          </button>
-          <button
-            ref={buttonRefs[1]}
-            aria-label="next step button"
-            tabIndex={0}
-            type="button"
-            onClick={() => {
-              handleNext(steps);
-            }}
-            onKeyDown={() => {
-              horizontalFocus(event);
-            }}
-            className={`${props.styledButtons2}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-4 h-4"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 5l-7 7m0 0l7 7m-7-7h18" />
+              </svg>
+            </button>
+            <button
+              ref={buttonRefs[1]}
+              aria-label="next step button"
+              tabIndex={0}
+              type="button"
+              onClick={() => {
+                handleNext(steps);
+              }}
+              onKeyDown={() => {
+                horizontalFocus(event);
+              }}
+              className="md:place-self-center  flex items-center buttonClass2 justify-center px-4 py-2 rounded-md hover:translate-x-1 transform md:transition-transform duration-200 md:ml-[20vw]"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </>
